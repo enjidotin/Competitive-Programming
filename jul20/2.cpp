3d8b2805046a4821424be5c0952027d801ed08b3
SuperBaniya <hydrogennaman@gmail.com>
2020-07-11 02:38:05 +0530

    ss

---
+++ jul20/2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Sum(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
+}
+int main()
+{
+    return 0;
+}
