9a2a4e2e4b6e07a56fd4b30267eb21c468bc7d3c
SuperBaniya <hydrogennaman@gmail.com>
2020-07-12 00:49:34 +0530

    e

---
+++ jul20/3.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int t, a, b, c, d, i;
+    cin >> t;
+    while (t--)
+    {
+        b = 64;
+        cin >> a;
+        if (a == 0)
+        {
+            for (i = 1; i <= b; i++)
+            {
+                cout << "X";
+                if (i % 8 == 0)
+                {
+                    cout << '\n';
+                }
+            }
+        }
+        if (a > 0)
+        {
+            cout << 'O';
+            a--;
+            b--;
+            while (a--)
+            {
+                b--;
+                cout << ".";
+                if (b % 8 == 0)
+                {
+                    cout << '\n';
+                }
+            }
+            while (b-- && b >= 0)
+            {
+                cout << "X";
+                if (b % 8 == 0)
+                {
+                    cout << '\n';
+                }
+            }
+        }
+        cout << "\n";
+    }
+}
