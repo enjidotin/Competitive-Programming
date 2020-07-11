5c87af5a6176014c2623c5dd8cd2ccd8cf3350b5
SuperBaniya <hydrogennaman@gmail.com>
2020-07-11 23:42:52 +0530

    gg

--- jul20/2.cpp
+++ jul20/2.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int Sum(int n)
 {
@@ -12,5 +12,47 @@ int Sum(int n)
 }
 int main()
 {
+    long long int t, a, b, c, d, cw, mw, cc, mc;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a;
+        mw = 0;
+        cw = 0;
+        while (a--)
+        {
+            cin >> c >> d;
+            cc = Sum(c);
+            mc = Sum(d);
+            if (cc > mc)
+            {
+                cw++;
+            }
+            if (cc < mc)
+            {
+                mw++;
+            }
+            if (cc == mc)
+            {
+                cw++;
+                mw++;
+            }
+        }
+        if (mw > cw)
+        {
+            cout << "1 " << mw;
+        }
+
+        if (mw < cw)
+        {
+            cout << "0 " << cw;
+        }
+        if (mw == cw)
+        {
+            cout << "2 " << mw;
+        }
+        cout << "\n";
+    }
+
     return 0;
 }
